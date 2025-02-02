ccec771be250db3f5b1c969fa3ae31b4bc4da3ca
Stefan Paul Noack <stefan@noack.software>
2025-02-02 02:26:27 +0200

    implement memory_release

--- rom/test/memory.asm
+++ rom/test/memory.asm
@@ -4,9 +4,9 @@
 ; ************************************
 
 ; Interactive test:
-; d <addr> <size>; 	- dump memory
-; a <size>;		- allocate memory
-; r <addr> <size>;	- release memory
+; D <addr> <size>; 	- dump memory
+; A <size>;		- allocate memory
+; R <addr> <size>;	- release memory
 
 
 ; **********************
@@ -29,96 +29,101 @@
 	acall	serial_init
 
 	; use xram
-	mov	a,	auxr
-	anl	a,	0xe1	; clear extram, xrs0-2
-	orl	a,	0x10	; set xrs2 (size 1792 bytes)
-	mov	auxr,	a
+	mov	a,auxr
+	anl	a,0xe1	; clear extram, xrs0-2
+	orl	a,0x10	; set xrs2 (size 1792 bytes)
+	mov	auxr,a
 
 	; initialize memory list
-	mov	dptr,	#0000
-	mov	memory_list,	dpl
-	mov	memory_list+1,	dph
+	mov	dptr,#0000
+	mov	memory_list,dpl
+	mov	memory_list+1,dph
 	; list head: pointer to first block
-	mov	a,	#4
-	movx	@dptr,	a
+	mov	a,#4
+	mov	memory_start,a
+	movx	@dptr,a
 	inc	dptr
 	clr	a
-	movx	@dptr,	a
+	mov	memory_start+1,a
+	movx	@dptr,a
 	inc	dptr
+	; max address of allocatable memory: 0x06ff
+	mov	memory_end,#0xff
+	mov	memory_end+1,0x06
 	; amount of free memory: 1792-4 = 1788 = 0x06fc
-	mov	a,	#0xfc
-	movx	@dptr,	a
+	mov	a,#0xfc
+	movx	@dptr,a
 	inc	dptr
-	mov	a,	#0x06
-	movx	@dptr,	a
+	mov	a,#0x06
+	movx	@dptr,a
 	inc	dptr
 	; first entry: pointer to next (null initially)
 	clr	a
-	movx	@dptr,	a
+	movx	@dptr,a
 	inc	dptr
-	movx	@dptr,	a
+	movx	@dptr,a
 	inc	dptr
 	; size of block (same as free memory initially)
-	mov	a,	#0xfc
-	movx	@dptr,	a
+	mov	a,#0xfc
+	movx	@dptr,a
 	inc	dptr
-	mov	a,	#0x06
-	movx	@dptr,	a
+	mov	a,#0x06
+	movx	@dptr,a
 	inc	dptr
 
 loop:
 	acall	read_char
 
-	cjne	a,	#'d',	__1
+	cjne	a,#'D',__1
 	acall	read_hex_32
-	mov	stream_in,	r0
-	mov	stream_in+1,	r1
+	mov	stream_in,r0
+	mov	stream_in+1,r1
 	acall	read_hex_32
-	mov	a,	r0
-	mov	r0,	stream_in
-	mov	r1,	stream_in+1
-	mov	r2,	#0
-	mov	r3,	#0
+	mov	a,r0
+	mov	r0,stream_in
+	mov	r1,stream_in+1
+	mov	r2,#0
+	mov	r3,#0
 	push	in
 	push	in+1
-	mov	dptr,	#stream_xram_read
-	mov	in,	dpl
-	mov	in+1,	dph
+	mov	dptr,#stream_xram_read
+	mov	in,dpl
+	mov	in+1,dph
 	acall	dump
 	pop	in+1
 	pop	in
 	sjmp	loop
 __1:
-	cjne	a,	#'a',	__2
+	cjne	a,#'A',__2
 	acall	read_hex_32
 	acall	memory_allocate
-	acall	print_hex_32
+	acall	print_hex_16
 	sjmp	next
 __2:
-	cjne	a,	#'r',	__3
+	cjne	a,#'R',__3
 	acall	read_hex_32
-	mov	a,	r0
-	mov	r4,	a
-	mov	a,	r1
-	mov	r5,	a
+	mov	a,r0
+	mov	r4,a
+	mov	a,r1
+	mov	r5,a
 	acall	read_hex_32
-	mov	a,	r0
-	mov	r2,	a
-	mov	a,	r1
-	mov	r3,	a
-	mov	a,	r4
-	mov	r0,	a
-	mov	a,	r5
-	mov	r1,	a
+	mov	a,r0
+	mov	r2,a
+	mov	a,r1
+	mov	r3,a
+	mov	a,r4
+	mov	r0,a
+	mov	a,r5
+	mov	r1,a
 	acall	memory_release
 	sjmp	next
 __3:
-	mov	a,	#'?'
+	mov	a,#'?'
 	acall	print_char
 next:
-	mov	a,	#13
+	mov	a,#13
 	acall	print_char
-	mov	a,	#10
+	mov	a,#10
 	acall	print_char
 	sjmp	loop
 
--- rom/test/memory.t.py
+++ rom/test/memory.t.py
@@ -7,44 +7,66 @@ from loader import *
 
 def run_test(pe):
         pe.logfile_read.raw=True
-        pe.send("d0;10;")
+        pe.send("D0;10;")
         pe.expect("00000000: 04 00 fc 06  00 00 fc 06")
         pe.expect("\r\n")
 
         # allocate first block
-        pe.send("a123;")
-        pe.expect("00000004\r\n")
-        pe.send("d0;10;")
+        pe.send("A0123;")
+        pe.expect("0004\r\n")
+        pe.send("D0000;10;")
         pe.expect("00000000: 28 01 d8 05")
         pe.expect("\r\n")
-        pe.send("d128;10;")
+        pe.send("D0128;10;")
         pe.expect("00000128: 00 00 d8 05")
         pe.expect("\r\n")
 
         # allocate another block
-        pe.send("a312;")
-        pe.expect("00000128\r\n")
-        pe.send("d0;10;")
+        pe.send("A0312;")
+        pe.expect("0128\r\n")
+        pe.send("D0000;10;")
         pe.expect("00000000: 3c 04 c4 02")
         pe.expect("\r\n")
-        pe.send("d43c;10;")
+        pe.send("D043c;10;")
         pe.expect("0000043c: 00 00 c4 02")
         pe.expect("\r\n")
 
         # allocate third block
-        pe.send("a1c4;")
-        pe.expect("0000043c\r\n")
-        pe.send("d0;10;")
+        pe.send("A01c4;")
+        pe.expect("043c\r\n")
+        pe.send("D0000;10;")
         pe.expect("00000000: 00 06 00 01")
         pe.expect("\r\n")
 
         # release second block
-        pe.send("r128;312;")
-        pe.send("d0;10;")
-        pe.expect("00000000: 28 01 12 04")
+        pe.send("R0128;0312;")
         pe.expect("\r\n")
-        pe.send("d128;10;")
-        pe.expect("00000128: 00 06 12 03")
+        pe.send("D0000;10;")
+        pe.expect("00000000: 28 01 14 04")
+        pe.expect("\r\n")
+        pe.send("D0128;10;")
+        pe.expect("00000128: 00 06 14 03")
+        pe.expect("\r\n")
+
+        # release third block
+        pe.send("R043c;01c4;")
+        pe.expect("\r\n")
+        pe.send("D0128;10;")
+        pe.expect("00000128: 00 00 d8 05")
+        pe.expect("\r\n")
+
+        # release part of first block
+        pe.send("R0004;0100;")
+        pe.expect("\r\n")
+        pe.send("D0000;10;")
+        pe.expect("00000000: 04 00 d8 06  28 01 00 01")
+        pe.expect("\r\n")
+
+        # release everything
+        pe.send("R0104;0023;")
+        pe.expect("\r\n")
+        pe.send("D0000;10;")
+        pe.expect("00000000: 04 00 fc 06  00 00 fc 06")
         pe.expect("\r\n")
 
 
